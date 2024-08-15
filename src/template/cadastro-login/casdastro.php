82b446741c856e9d08b490a62476ba226071b4b3
Gabryell <gabryellcostademoura1@gmail.com>
2024-08-15 16:17:13 -0300

    Alterando os campos do formulario de cadastro

--- src/template/cadastro-login/casdastro.php
+++ src/template/cadastro-login/casdastro.php
@@ -17,37 +17,8 @@
             <form action="#" class="cadastro-form">
                 <h2>Cadastro</h2>
                 <div class="form-group">
-                    <label for="nome"class="texto-informacao">Nome completo:</label>
-                    <input type="text" id="nome" name="nome" placeholder="exemplo: João Eduardo Jacoby de Oliveira">
-                </div>
-                <div class="form-group">
-                    <label for="gênero"class="texto-informacao">Gênero:</label>
-                    <input type="radio" class="gênero" name="gênero" value="F">
-                    <span>Feminino</span>
-                    <input type="radio" class="gênero" name="gênero" value="M">
-                    <span>Masculino</span>
-                    <input type="radio" class="gênero" name="gênero" value="H">
-                    <span>Helicopter</span>
-                </div>
-                <div class="form-group">
-                    <label for="nascimento"class="texto-informacao">Data de Nascimento:</label>
-                    <input type="text" id="nascimento" name="nascimento" placeholder="exemplo: 01/01/0001" type="tel" maxlength="10">
-                </div>
-                <div class="form-group">
-                    <label for="cpf"class="texto-informacao">CPF:</label>
-                    <input type="tel" id="cpf" name="cpf" placeholder="exemplo: 000.000.000-00">
-                </div>
-                <div class="form-group">
-                    <label for="telefone"class="texto-informacao">Telefone:</label>
-                    <input type="tel" id="telefone" name="telefone" placeholder="exemplo: (00) 90000-0000">
-                </div>
-                <div class="form-group">
-                    <label for="username"class="texto-informacao">Usuário:</label>
-                    <input type="text" id="username" name="username" placeholder="exemplo: Joãozinho123">
-                </div>
-                <div class="form-group">
-                    <label for="email"class="texto-informacao">E-mail:</label>
-                    <input type="email" id="email" name="email" placeholder="exemplo: joãozinho123@gmail.com">
+                    <label for="username"class="texto-informacao">Nome de Usuário:</label>
+                    <input type="txt" id="username" name="username" placeholder="exemplo: Joãozinho123">
                 </div>
                 <div class="form-group">
                     <label for="password"class="texto-informacao">Senha:</label>
@@ -56,6 +27,39 @@
                 <div class="form-group">
                     <label for="password"class="texto-informacao">Senha(novamente):</label>
                     <input type="password" id="password2" name="password2" placeholder="Digite a senha(novamente)">
+                </div>
+                <div class="form-group">
+                    <label for="nome"class="texto-informacao">Primeiro Nome:</label>
+                    <input type="name" id="nome" name="primeiro_nome" placeholder="exemplo: João Eduardo">
+                </div>
+                <div class="form-group">
+                    <label for="nome"class="texto-informacao">Sobrenome:</label>
+                    <input type="text" id="nome" name="sobrenome" placeholder="exemplo:Jacoby de Oliveira">
+                </div>
+                <div class="form-group">
+                    <label for="nascimento"class="texto-informacao">Data de Nascimento:</label>
+                    <input type="date" id="nascimento" name="nascimento" placeholder="exemplo: 01/01/2001" type="tel" maxlength="10">
+                </div>
+                <div class="form-group">
+                    <label for="email"class="texto-informacao">E-mail:</label>
+                    <input type="email" id="email" name="email" placeholder="exemplo: joãozinho123@gmail.com">
+                </div>
+                <div class="form-group">
+                    <label for="telefone"class="texto-informacao">Telefone:</label>
+                    <input type="tel" id="telefone" name="telefone" placeholder="exemplo: (00) 90000-0000">
+                </div>
+                <div class="form-group">
+                    <label for="genero"class="texto-informacao">Gênero:</label>
+                    <input type="radio" class="gênero" name="genero" value="F">
+                    <span>Feminino</span>
+                    <input type="radio" class="gênero" name="genero" value="M">
+                    <span>Masculino</span>
+                    <input type="radio" class="gênero" name="genero" value="H">
+                    <span>Outro</span>
+                </div>
+                <div class="form-group">
+                    <label for="cpf"class="texto-informacao">CPF:</label>
+                    <input type="number" id="cpf" name="cpf" placeholder="exemplo: 000.000.000-00">
                 </div>
                 <div class="form-group">
                     <div class="links-auxiliares">
